d1b5daa0c91d82622ad452c79147f68038c2690b
ana_berthel <ana.berthel@yale.edu>
2021-07-26 18:27:19 -0400

    implemented arrow-key input

--- scripts/main.gd
+++ scripts/main.gd
@@ -11,24 +11,79 @@ extends Node2D
 # for reference
 const tileSize = 32
 
+# button press codes
+const PRESS_UP = 1
+const PRESS_DOWN = 2
+const PRESS_RIGHT = 3
+const PRESS_LEFT = 4
+
 ### VARIABLES ###
+
+# TODO: will need more granular locks eventually
+# ex. lock player movement, but allow button press to advance text.
+# ex. lock player movement, but allow inventory exploration
+
+# set to true to ignore all player input
+var allInputLocked = false
+
+
+
+# entities
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = self.get_node("player")
 
 ### UTILITY FUNCTIONS ###
 
-# convert map coordinates to screen coordinates
-func map2screen(pos):
-	return Vector2(int(pos.x/tileSize), int(pos.y/tileSize))
-
 # convert screen coordinates to map coordinates
 func screen2map(pos):
+	return Vector2(int(pos.x/tileSize), int(pos.y/tileSize))
+
+# convert map coordinates to screen coordinates
+func map2screen(pos):
 	return Vector2(pos.x*32, pos.y*32)
 
+### PLAYER INPUT FUNCTIONS ###
+func get_player_input():
+	if !allInputLocked:
+		## TODO: what is priority order for inputs?
+		
+		if Input.is_action_pressed("ui_up"):
+			print("up detected")
+			return PRESS_UP
+		elif Input.is_action_pressed("ui_down"):
+			return PRESS_DOWN
+		elif Input.is_action_pressed("ui_left"):
+			return PRESS_LEFT
+		elif Input.is_action_pressed("ui_right"):
+			return PRESS_RIGHT
+		else:
+			return -1
+	else:
+		return -1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+# called every frame
+func _process(delta):
+	var input = get_player_input()
+	
+	# if there was player input, carry it out
+	if input > 0:
+		
+		## TODO: check mode first
+		if input == PRESS_UP:
+			##TODO: need to check with map that this is a legal move
+			allInputLocked = true
+			player.move_up()
+		elif input == PRESS_DOWN:
+			allInputLocked = true
+			player.move_down()
+		elif input == PRESS_LEFT:
+			allInputLocked = true
+			player.move_left()
+		elif input == PRESS_RIGHT:
+			allInputLocked = true
+			player.move_right()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,59 @@
 extends Node2D
 
+## VARIABLES
 
+#position of player in map coordinates
 var pos = Vector2(1, 1)
-var tileSize
+
+#main node
+var main
+
+# timers
+# TODO: may not be best here
+var animationTimer = 0
+var animationTimerMax = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	# store tileSize here so we can reference it easily later
-	tileSize = self.get_parent().tileSize
+	main = self.get_parent()
+
+## MOVEMENT FUNCTIONS
+
+func move_up():
+	animationTimer = animationTimerMax
+	pos.y = pos.y - 1
+	position = main.map2screen(pos)
+	print(pos)
+
+
+func move_down():
+	animationTimer = animationTimerMax
+	pos.y = pos.y + 1
+	position = main.map2screen(pos)
+	print(pos)
+
+func move_left():
+	animationTimer = animationTimerMax
+	pos.x = pos.x - 1
+	position = main.map2screen(pos)
+	print(pos)
+
+func move_right():
+	animationTimer = animationTimerMax
+	pos.x = pos.x + 1
+	position = main.map2screen(pos)
+	print(pos)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	
+	# countdown animationTimer if it has been set
+	# if it reaches 0, reset the clock
+	if animationTimer > 0:
+		animationTimer = animationTimer - delta
+		
+		if animationTimer < 0:
+			main.allInputLocked = false
